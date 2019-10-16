1dee44da6c4a3e371bd9e9727908a18f0e500e5c
jawhnypoh <po.johnny@gmail.com>
2019-10-15 18:16:45 -0700

    Item details API called and loads when DetailedItem route loads

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class EbaySearchState extends State<EbaySearch> {
   final TextEditingController _query = TextEditingController();
   var dio = Dio();
   List items = List();
-  String _queryURL = 'https://api.ebay.com/buy/browse/v1/item_summary/search?';
+  final String _queryURL = 'https://api.ebay.com/buy/browse/v1/item_summary/search?';
   Widget _appTitle = Text('eBay Search App');
   Icon _queryIcon = Icon(Icons.search);
 
@@ -106,15 +106,9 @@ class EbaySearchState extends State<EbaySearch> {
 
   // Build the query URL based on what user entered
   String _buildQueryURL(String _queryText) {
-    print('_buildQueryURL _queryText: ' + _queryText);
     final String _finalURL = _queryURL + 'q=' + _queryText + '&limit=100';
     print('finalURL is: ' + _finalURL);
     return _finalURL;
-  }
-
-  // Build the item detailed URL based on itemID
-  String _buildItemURL() {
-
   }
 
   // Get results from eBay API with query text
@@ -124,7 +118,6 @@ class EbaySearchState extends State<EbaySearch> {
     try {
       final Response response = await dio.get<void>(_buildQueryURL(_queryText));
       print(response);
-
       List resultsList = List();
       for(int i=0; i<response.data['itemSummaries'].length; i++) {
         resultsList.add(response.data['itemSummaries'][i]);
@@ -139,12 +132,17 @@ class EbaySearchState extends State<EbaySearch> {
   }
 }
 
-class DetailedItem extends StatelessWidget {
-  // Declare itemId that holds itemId
-  final String itemId;
+class DetailedItemState extends State<DetailedItem> {
+  String itemId;
+  DetailedItemState(this.itemId);
 
-  // Require an ItemId in the constructor
-  DetailedItem({Key key, @required this.itemId}) : super(key: key);
+  @override
+  void initState() {
+    this._getItemResults(itemId);
+  }
+
+  final String _itemURL = 'https://api.ebay.com/buy/browse/v1/item/';
+  var dio = Dio();
 
   @override
   Widget build(BuildContext context) {
@@ -157,9 +155,40 @@ class DetailedItem extends StatelessWidget {
       ),
     );
   }
+
+  // Build the item detailed URL based on itemID
+  String _buildItemURL(String itemId) {
+    final String _finalURL = _itemURL + itemId;
+    print('finalURL is: ' + _finalURL);
+    return _finalURL;
+  }
+
+  void _getItemResults(String itemId) async {
+    print('_getItemResults() called ');
+    final String authToken = 'Bearer v^1.1#i^1#p^1#r^0#I^3#f^0#t^H4sIAAAAAAAAAOVYW2gUVxjObjZpxUZFRaPYuh0rKYadPWfvM7oLm5vZmsvqbtIY1DiXM9lJdmeWOWebbLE0Ro19aCuEgrGiCCkUKX3QeoM+1Jda9cHaQimpUhSh9AYFK7XgpZ2ZXeMmlcSaxQa6L8v85z//+f7v+/9zzgwYKJ+zZqhx6HaF5RnrkQEwYLVY4Fwwp7ysel6pdXlZCShwsBwZeGnANlj64zrMpZJpdhPCaVXByN6fSiqYNY1BKqMprMphGbMKl0KYJQIbCzc3sS4asGlNJaqgJil7pC5IQQ8PIHBJrgDyMAIPdKvyIGZcDVJ+BAS3i+NFABjeI4j6OMYZFFEw4RQSpFwAMg4IHNAXB4B1u1jgpf1e0EnZ25GGZVXRXWhAhUy4rDlXK8A6NVQOY6QRPQgVioQbYq3hSF19S3ydsyBWKM9DjHAkgyc+1aoisrdzyQyaehlserOxjCAgjClnKLfCxKBs+AGYJ4BvUu3nGBfyBSTGL0JRZ7MoVDaoWoojU+MwLLLokExXFilEJtnpGNXZ4HuQQPJPLXqISJ3d+NuY4ZKyJCMtSNXXhDeHo1Eq9IqaUJRsVHXolMtKd23CEd1U53DzbihB4BEdyOdxByRRyC+Ui5anedJKtaoiygZp2N6ikhqko0aTuXEVcKM7tSqtWlgiBqJCP9cDDj2eTkPUnIoZklAMXVFKJ8JuPk6vwPhsQjSZzxA0HmHygElRkOLSaVmkJg+atZgvn34cpBKEpFmns6+vj+5z06rW7XQBAJ0dzU0xIYFSHKX7Gr2e85enn+CQzVQEpM/EMkuyaR1Lv16rOgClmwp5APS7A3neJ8IKTbb+w1CQs3NiRxSrQ5AfQCi4OTeC0Md5uWJ0SChfpE4DB+K5rCPFab2IpJOcgByCXmeZFNJkkXV7JZdepMgh+hjJ4WEkycF7RZ8DSggBhHheYAL/p0Z53FKPIUFDpCi1XrQ6j2blXlTf0dHnrW70RevDm1/114iS0hmrrt3Qtomp7+j0BfiajXiDvzv4uN3w6OQFNY2ialIWskVgwOj1IrLg1sQop5FsDCWTumFGiWIj0dklsjEf6wG4tEwbjU0LasqpcvqObpi6TMQzyjmcTkdSqQzh+CSKFGc3/4928kemJ+t3nVmVk65fTkhZzF1SaFNNGr8m0BrCakbT72d0q3Fmx9VepOg7INHUZBJp7XDGQj9tfY1en4aPf3lYPFnuxbupzKbaFpKyXkJdsy2zp6KozM2y0xh6/RAGvIzLO6O8ak1N49nZdg41qpggcarUbOuf8FrtnPiSHyoxf3DQchIMWo5ZLRbgBKvhKvBieWmbrfS55VgmiJY5icZyt6K/u2qI7kXZNCdr1nKLPPz13m8KPisc2Qoqxz8szCmFcwu+MoAVD0fK4PylFZCBAPoAcLuAtxOsejhqg0tsi+1XW4ai23Y2nTu6rKr9rfekZ4e2fAIqxp0slrIS26ClhDn2/o3T+9dc/L7xfC35NjTv/raFP30Zd94cHW37dXjl2NzPhUXUrfWe148uraraSiLHL5y52jx8/t69tjp1d6Xnsu3gz6NdXR6pYQEWGr57Y2wkfmN0F1O59u7uM22LLyZ2jlyvTPyAr97sWrll++IDJ5rZ+ddbP/tr7O5vlzoGD+0Ff1y60gNG7n28urny1v5Fb795pXrhuoNnVsBTfnSK6e554dqyrw5du3z2wx0Vgzd/X/4nv+NOQ5O2dgSfGL5DLzxbdemjji9OVy3ped5y4eWjp603Vp7c8wtYffv4ucORQ0nuHXzgWuTAu3JneMHhD85bPQfBvl0X1+4hvbc+bd9/+f7Y9n1Mf2NOvr8BHf0NLfARAAA=';
+    dio.options.headers = {'Authorization' : authToken};
+    try {
+      final Response response = await dio.get<void>(_buildItemURL(itemId));
+      print(response);
+    }
+    catch (e) {
+      print(e);
+    }
+  }
 }
 
 class EbaySearch extends StatefulWidget {
   @override
   EbaySearchState createState() => EbaySearchState();
+}
+
+class DetailedItem extends StatefulWidget {
+  // Declare itemId that holds itemId
+  final String itemId;
+
+  // Require an ItemId in the constructor
+  DetailedItem({Key key, @required this.itemId}) : super(key: key);
+
+  @override
+  DetailedItemState createState() => DetailedItemState(itemId);
 }
